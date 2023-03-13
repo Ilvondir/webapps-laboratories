b8d215a3e40355de313e801de7f586b9b5b5ad04
Michał Komsa <101294147+Ilvondir@users.noreply.github.com>
2023-03-13 08:59:58 +0100

    Add files via upload

---
+++ Lab2/Zad14.php
@@ -0,0 +1,22 @@
+<?php
+    use Ramsey\Uuid\Uuid;
+    
+    class Dog {
+        private $id;
+
+        public function __construct(
+            private string $nazwa,
+            private int $wiek,
+            private string $dataPrzyjecia
+        ) {
+            $this->id = Uuid::uuid4();
+        }
+
+        public function __toString() {
+            return $this->nazwa. "(". $this->wiek. " l.) przyjety w dn. ". $this->dataPrzyjecia. ".".PHP_EOL;
+        }
+    }
+
+    $d1 = new Dog("Fifek", 3, "12-02-2023");
+    echo $d1->__toString();
+?>
