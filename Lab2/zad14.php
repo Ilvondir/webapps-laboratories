84c1bd6f5f9d2ca9d059bb3396882b4ce2d2752e
Michał Komsa <101294147+Ilvondir@users.noreply.github.com>
2023-03-13 16:37:56 +0100

    Add files via upload

--- Lab2/zad14.php
+++ Lab2/zad14.php
@@ -1,5 +1,6 @@
 <?php
-    use Ramsey\Uuid\Uuid;
+require_once __DIR__."/vendor/autoload.php";
+use Ramsey\Uuid\Uuid as Uuid;
     
     class Dog {
         private $id;
@@ -8,15 +9,22 @@
             private string $nazwa,
             private int $wiek,
             private string $dataPrzyjecia
-        ) {
+        ) {}
+
+        public function setId() {
             $this->id = Uuid::uuid4();
         }
 
         public function __toString() {
-            return $this->nazwa. "(". $this->wiek. " l.) przyjety w dn. ". $this->dataPrzyjecia. ".".PHP_EOL;
+            return $this->nazwa. "(". $this->wiek. " l.) przyjety w dn. ". $this->dataPrzyjecia. ". ID: ". $this->id.PHP_EOL;
         }
     }
 
     $d1 = new Dog("Fifek", 3, "12-02-2023");
+    $d1->setId();
     echo $d1->__toString();
+
+    $d2 = new Dog("Lola", 7, "30-01-2023");
+    $d2->setId();
+    echo $d2->__toString();
 ?>
